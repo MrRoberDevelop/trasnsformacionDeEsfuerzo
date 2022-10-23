8a980292cb46daa218ccbe224a1bfb1fa89707e4
MrRober <mrrober.develop@gmail.com>
2022-10-23 01:36:21 -0500

    rebase

--- Vista.py
+++ Vista.py
@@ -22,26 +22,73 @@ class Vista:
         title.pack(pady = 10)
         firstName.pack(pady =10)
         self.createTable()
-        self.button()
-
-
-
+        self.print_data()
+        self.entries()
+         
         self.master.mainloop()
+
+
+    def entries(self):
+        self.frame = Frame(self.master)
+        self.frame.pack()
+
+        lista = [
+            "material",
+            "tipo",
+            "densidad",
+            "esfuerzo",
+            "modulo"
+        ]
+
+        for i in range(len(lista)):
+            l=Label(self.frame,text=lista[i])
+            l.grid(row=0,column=i)
+
+        self.material  = Entry(self.frame)
+        self.tipo = Entry(self.frame)
+        self.densidad = Entry(self.frame)
+        self.esfuerzo = Entry(self.frame)
+        self.modulo = Entry(self.frame)
+        self.addValues = Button(self.master,
+                                text="nuevo material",
+                                command=self.nuevo)
+
+        self.material.grid(row=1, column=0)
+        self.tipo.grid(row=1, column=1)
+        self.densidad.grid(row=1, column=2)
+        self.esfuerzo.grid(row=1, column=3)
+        self.modulo.grid(row=1, column=4)
+        self.addValues.pack()
+
+    def nuevo(self):
+        material={
+            "material"  :self.material.get(),
+            "tipo"      :self.tipo.get(),
+            "desidad"   :float(self.densidad.get()),
+            "esfuerzo"  :float(self.esfuerzo.get()),
+            "modulo"    :float(self.modulo.get())
+            }
+        
+
+        include_data(material)
+        self.print_data()
+
 
     def createTable(self):
         txt = Text(self.master)
         txt.pack()
 
         class PrintToTXT(object):
-            def write(self,s):
-                txt.insert(END,s)
-
             def flush(self):
                 pass
+            def write(self,s):
+                txt.insert("1.0",s)
         sys.stdout = PrintToTXT()
+
 
     def print_data(self):
         df = pd.DataFrame(data=create_data_table())
+        print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
         print(df)
 
     def button(self):
